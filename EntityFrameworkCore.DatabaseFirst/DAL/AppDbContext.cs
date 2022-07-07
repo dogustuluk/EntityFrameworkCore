a6b0947009d6713040d28809f70e32d1a341e671
dogustuluk <dogus.tuluk@gmail.com>
2022-07-07 19:46:41 +0300

    DbContext and DbSet

---
+++ EntityFrameworkCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.DatabaseFirst.DAL
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-1C9EMR6;Initial Catalog=EntityFrameworkCoreDatabaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
--- EntityFrameworkCore.DatabaseFirst/Program.cs
+++ EntityFrameworkCore.DatabaseFirst/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EntityFrameworkCore.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name}");
+    });
+}
