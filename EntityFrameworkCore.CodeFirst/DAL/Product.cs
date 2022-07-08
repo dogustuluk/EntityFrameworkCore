2e555a097af3ab869ab4a1a68afeacdd0b2fc2c4
dogustuluk <dogus.tuluk@gmail.com>
2022-07-08 17:49:51 +0300

    remove-migration and script-migration

--- EntityFrameworkCore.CodeFirst/DAL/Product.cs
+++ EntityFrameworkCore.CodeFirst/DAL/Product.cs
@@ -11,7 +11,7 @@ namespace EntityFrameworkCore.CodeFirst.DAL
         public int Id { get; set; }
         public string Name { get; set; }
         public int Stock { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         public int Barcode { get; set; }
     }
 }
--- EntityFrameworkCore.CodeFirst/Migrations/AppDbContextModelSnapshot.cs
+++ EntityFrameworkCore.CodeFirst/Migrations/AppDbContextModelSnapshot.cs
@@ -44,7 +44,7 @@ namespace EntityFrameworkCore.CodeFirst.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Products");
+                    b.ToTable("Products", (string)null);
                 });
 #pragma warning restore 612, 618
         }
