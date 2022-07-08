75f1eb348d747d26e812d35bb57c0d2d0c48ebdc
dogustuluk <dogus.tuluk@gmail.com>
2022-07-08 16:18:07 +0300

    appdbcontext and product classes are created

---
+++ EntityFrameworkCore.CodeFirst/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.CodeFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+    }
+   
+}
