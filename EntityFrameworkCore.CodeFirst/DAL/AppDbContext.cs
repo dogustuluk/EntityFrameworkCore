2579e06b1a4fe8d03b94658456debcc5d20e6742
dogustuluk <dogus.tuluk@gmail.com>
2022-07-08 16:29:19 +0300

    various update on AppDbContext, appsettings.json, Program.cs

--- EntityFrameworkCore.CodeFirst/DAL/AppDbContext.cs
+++ EntityFrameworkCore.CodeFirst/DAL/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace EntityFrameworkCore.CodeFirst.DAL
         public DbSet<Product> Products { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("");
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
         }
 
     }
--- EntityFrameworkCore.CodeFirst/Program.cs
+++ EntityFrameworkCore.CodeFirst/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntityFrameworkCore.CodeFirst;
+using EntityFrameworkCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+
+using (var context = new AppDbContext())
+{
+    var products = await context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock}");
+    });
+}
