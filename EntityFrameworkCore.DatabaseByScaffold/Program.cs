ef9cd0c802ebdf7834db10cfcdeefdeeb8a5e657
dogustuluk <dogus.tuluk@gmail.com>
2022-07-08 15:19:06 +0300

    Scaffold process

---
+++ EntityFrameworkCore.DatabaseByScaffold/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EntityFrameworkCore.DatabaseByScaffold.Models;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+using (var context = new EntityFrameworkCoreDatabaseFirstDbContext())
+{
+    var products = await context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} TL - {p.Stock}");
+    });
+}
