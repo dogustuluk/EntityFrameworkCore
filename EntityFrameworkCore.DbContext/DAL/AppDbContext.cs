34155b435b7344625531b511ec35cc9ea7d5fd07
dogustuluk <dogus.tuluk@gmail.com>
2022-07-09 11:13:37 +0300

    DbContext project created

---
+++ EntityFrameworkCore.DbContext/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.CodeFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build(); //Console uygulamasında bu kod alttaki koddan önce mutlaka yazılması gerekmektedir. Sebebi ise alttaki metottan önce bu metot çalışsın ki migration sırasında connectionString'i okuyabilsin.
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
+        }
+
+    }
+   
+}
---
+++ EntityFrameworkCore.DbContext/Program.cs
@@ -0,0 +1,14 @@
+﻿using EntityFrameworkCore.CodeFirst;
+using EntityFrameworkCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+
+using (var context = new AppDbContext())
+{
+    var products = await context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock}");
+    });
+}
