6d09db3dbc4789c9d6a5f84bd0052556b51bc397
dogustuluk <dogus.tuluk@gmail.com>
2022-07-11 15:31:33 +0300

    relationship mant-to-many

--- EntityFrameworkCore.Relationships/DAL/AppDbContext.cs
+++ EntityFrameworkCore.Relationships/DAL/AppDbContext.cs
@@ -11,8 +11,9 @@ namespace EntityFrameworkCore.CodeFirst.DAL
 {
     public class AppDbContext:DbContext
     {
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; } //Eğer veri tabanında AppDbContext aracılığıyla herhangi bir işlem yapılmasını istemiyorsak bu satırı silebiliriz.
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; } //Eğer veri tabanında AppDbContext aracılığıyla herhangi bir işlem yapılmasını istemiyorsak bu satırı silebiliriz.
+        //many-to-many için one-to-many içeren iki tablonun birbirlerine navigate edilmesi gerekmektedir. EF Core ortaya çıkacak olan yeni çoka çok tabloyu kendisi otomatik olarak oluşturacaktır.
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             Initializer.Build(); //Console uygulamasında bu kod alttaki koddan önce mutlaka yazılması gerekmektedir. Sebebi ise alttaki metottan önce bu metot çalışsın ki migration sırasında connectionString'i okuyabilsin.
@@ -38,8 +39,20 @@ namespace EntityFrameworkCore.CodeFirst.DAL
 
             //one-to-one best practise start
             //Child tablosundaki primary key alanını ek olarak foreign key olarak da kullanmalıyız. Eğer bunu yaparsak otomatik artan özelliğini de kapatmamız gerekmektedir lakin EF Core bunu bizim için otomatik olarak yapmış olmaktadır.
-            modelBuilder.Entity<Product>().HasOne(x => x.ProductFeature).WithOne(x => x.Product).HasForeignKey<ProductFeature>(x => x.Id); //Bu şekilde yaparsak one-to-one ilişkisindeki best practise'i yakalamış oluruz.
+            //modelBuilder.Entity<Product>().HasOne(x => x.ProductFeature).WithOne(x => x.Product).HasForeignKey<ProductFeature>(x => x.Id); //Bu şekilde yaparsak one-to-one ilişkisindeki best practise'i yakalamış oluruz.
             //best practise end
+
+            //many-to-many start
+            //Custom many-to-many table on fluent api
+            modelBuilder.Entity<Student>()
+                .HasMany(x => x.Teachers)
+                .WithMany(x => x.Students)
+                .UsingEntity<Dictionary<string, object>>(
+                "CustomManyToManyTable",
+                x=> x.HasOne<Teacher>().WithMany().HasForeignKey("Teacher_Id").HasConstraintName("FK__TeacherId"),
+                x=> x.HasOne<Student>().WithMany().HasForeignKey("Student_Id").HasConstraintName("FK__StudentId")
+                );
+            //end
             base.OnModelCreating(modelBuilder);
         }
 
--- EntityFrameworkCore.Relationships/Program.cs
+++ EntityFrameworkCore.Relationships/Program.cs
@@ -10,11 +10,11 @@ using (var context = new AppDbContext())
     //one to many (bire çok) ilişkide (örnekte category - bir, product - çok) category'den bir satır silinirse o kategoriye sahip tüm product'lar da silinir.>>default davranışı<<<<<
 
 
-    context.Products.Add(new Product { Name = "Lamy Safari Blue", Price = 550, Stock = 25, Barcode = 563 });
-    context.Products.Add(new Product { Name = "Lamy Safari Red", Price = 550, Stock = 15, Barcode = 564 });
-    context.Products.Add(new Product { Name = "Lamy Safari Green", Price = 550, Stock = 5, Barcode = 565 });
+    //context.Products.Add(new Product { Name = "Lamy Safari Blue", Price = 550, Stock = 25, Barcode = 563 });
+    //context.Products.Add(new Product { Name = "Lamy Safari Red", Price = 550, Stock = 15, Barcode = 564 });
+    //context.Products.Add(new Product { Name = "Lamy Safari Green", Price = 550, Stock = 5, Barcode = 565 });
    
-    var products = await context.Products.ToListAsync();
+    //var products = await context.Products.ToListAsync();
 
     
     context.SaveChanges();
