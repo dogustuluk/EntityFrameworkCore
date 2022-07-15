ac5e29aff7ec7fbbb86a76acdc69b58f38325138
dogustuluk <dogus.tuluk@gmail.com>
2022-07-15 15:04:36 +0300

    Related Data Loads

---
+++ EntityFrameworkCore.RelatedDataLoads/DAL/Product.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkCore.Relationships.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.CodeFirst.DAL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int Kdv { get; set; }
+        public int Barcode { get; set; }
+
+        public decimal PriceKdv { get; set; }
+        public Category Category { get; set; }
+        public ProductFeature ProductFeature { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ EntityFrameworkCore.RelatedDataLoads/Program.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkCore.CodeFirst;
+using EntityFrameworkCore.CodeFirst.DAL;
+using EntityFrameworkCore.Relationships.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+
+using (var context = new AppDbContext())
+{
+                                            //Related Data Loads
+
+    //Eager loading,        Explicit loading,       Lazy loading
+    //Eager Loading
+        //Bir datayı çekerken, aynı anda ilgili datanın parent'ını da çekmek istediğimiz zaman buna "Eager Loading" denmektedir. Örnek::>> bir product'ı çekerken aynı anda product'a bağlı kategoriyi de çekmek denilebilir.
+
+
+    
+    Console.WriteLine("İşlem Başarılı");
+}
