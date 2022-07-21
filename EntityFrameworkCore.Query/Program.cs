4dfa27edac7da537eb93a2c83365dedaa7a6d914
dogustuluk <dogus.tuluk@gmail.com>
2022-07-21 14:44:19 +0300

    left-right join

--- EntityFrameworkCore.Query/Program.cs
+++ EntityFrameworkCore.Query/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EntityFrameworkCore.CodeFirst;
 using EntityFrameworkCore.CodeFirst.DAL;
 using EntityFrameworkCore.Relationships.DAL;
+using Microsoft.EntityFrameworkCore;
 
 Initializer.Build();
 
@@ -40,76 +41,104 @@ using (var context = new AppDbContext())
     //context.SaveChanges();
 
     //2'li join start
-    var result = context.Categories.Join(context.Products, x => x.Id, y => y.CategoryId,(c, p) => new
-    {
-        CategoryName = c.Name,
-        ProductName = p.Name,
-        ProductPrice = p.Price
-    }).ToList();
+    //var result = context.Categories.Join(context.Products, x => x.Id, y => y.CategoryId,(c, p) => new
+    //{
+    //    CategoryName = c.Name,
+    //    ProductName = p.Name,
+    //    ProductPrice = p.Price
+    //}).ToList();
 
-    var resultProduct = context.Categories.Join(context.Products, x => x.Id, y => y.CategoryId, (c, p) => p).ToList();
-    var resultCategory = context.Categories.Join(context.Products, x => x.Id, y => y.CategoryId, (c, p) => c).ToList();
-
-
-    result.ForEach(x =>
-    {
-        Console.WriteLine($"{x.CategoryName} - {x.ProductName} - {x.ProductPrice}");
-    });
+    //var resultProduct = context.Categories.Join(context.Products, x => x.Id, y => y.CategoryId, (c, p) => p).ToList();
+    //var resultCategory = context.Categories.Join(context.Products, x => x.Id, y => y.CategoryId, (c, p) => c).ToList();
 
 
-    resultProduct.ForEach(x =>
-    {
-        Console.WriteLine($"{x.Name}");
-    });
-    resultCategory.ForEach(x =>
-    {
-        Console.WriteLine($"{x.Name}");
-    });
+    //result.ForEach(x =>
+    //{
+    //    Console.WriteLine($"{x.CategoryName} - {x.ProductName} - {x.ProductPrice}");
+    //});
 
-    //2.yol olarak ise aşağıdaki gibi yazabiliriz.
-    var result2 = (from c in context.Categories
-                   join p in context.Products on c.Id equals p.CategoryId
-                   select new
-                   {
-                       CategoryName = c.Name,
-                       ProductName = p.Name,
-                       ProductPrice = p.Price
-                   }).ToList();
 
-    //2'li join end
-    //3'lü join start
-    var resultTrilogy = context.Categories
-        .Join(context.Products, c => c.Id, p => p.CategoryId, (c, p) => new { c, p })
-        .Join(context.productFeatures, c => c.p.Id, pf => pf.Id, (c, pf) => new 
-        {
-            CategoryName = c.c.Name,
-            ProductName = c.p.Name,
-            ProductPrice = c.p.Price,
-            ProductColor = pf.Color
-        });
+    //resultProduct.ForEach(x =>
+    //{
+    //    Console.WriteLine($"{x.Name}");
+    //});
+    //resultCategory.ForEach(x =>
+    //{
+    //    Console.WriteLine($"{x.Name}");
+    //});
 
-    //2.yol >>>> Daha okunaklı olan, tavsiye edilen.
-    var resultTrilogy2 = (from c in context.Categories
-                          join p in context.Products on c.Id equals p.CategoryId
-                          join pf in context.productFeatures on p.Id equals pf.Id
-                          select new
-                          {
-                              categoryName = c.Name,
-                              productName = p.Name,
-                              productPrice = p.Price,
-                              productColor = pf.Color,
-                              productHeight = pf.Height
+    ////2.yol olarak ise aşağıdaki gibi yazabiliriz.
+    //var result2 = (from c in context.Categories
+    //               join p in context.Products on c.Id equals p.CategoryId
+    //               select new
+    //               {
+    //                   CategoryName = c.Name,
+    //                   ProductName = p.Name,
+    //                   ProductPrice = p.Price
+    //               }).ToList();
 
-                          }).ToList();
-    resultTrilogy2.ForEach(x =>
-    {
-        Console.WriteLine($"{x.categoryName} - {x.productName} - {x.productPrice} - {x.productColor} - {x.productHeight}");
-    });
+    ////2'li join end
+    ////3'lü join start
+    //var resultTrilogy = context.Categories
+    //    .Join(context.Products, c => c.Id, p => p.CategoryId, (c, p) => new { c, p })
+    //    .Join(context.productFeatures, c => c.p.Id, pf => pf.Id, (c, pf) => new 
+    //    {
+    //        CategoryName = c.c.Name,
+    //        ProductName = c.p.Name,
+    //        ProductPrice = c.p.Price,
+    //        ProductColor = pf.Color
+    //    });
+
+    ////2.yol >>>> Daha okunaklı olan, tavsiye edilen.
+    //var resultTrilogy2 = (from c in context.Categories
+    //                      join p in context.Products on c.Id equals p.CategoryId
+    //                      join pf in context.productFeatures on p.Id equals pf.Id
+    //                      select new
+    //                      {
+    //                          categoryName = c.Name,
+    //                          productName = p.Name,
+    //                          productPrice = p.Price,
+    //                          productColor = pf.Color,
+    //                          productHeight = pf.Height
+
+    //                      }).ToList();
+    //resultTrilogy2.ForEach(x =>
+    //{
+    //    Console.WriteLine($"{x.categoryName} - {x.productName} - {x.productPrice} - {x.productColor} - {x.productHeight}");
+    //});
     //3'lü join end
 
     //INNER JOIN END
 
+    //LEFT - RIGHT JOIN START
+    //Left Join >>> Bu join tipinde hem kesişen alanlar hem de sol tarafta bulunan tablonun tüm alanları alınmaktadır.
+    //Right Join >>> Bu join tipinde hem kesişen alanlar hem de sağ tarafta bulunan tablonun tüm alanları alınmaktadır.
+    //EF Core tarafında left ya da right join yoktur ama custom linq sorguları yazılarak yapılabilir. Bunların adı left - right join olarak adlandırılmaz.
 
+    var leftJoinResult = (from p in context.Products
+                  join pf in context.productFeatures on p.Id equals pf.Id into pfList
+                  from pf in pfList.DefaultIfEmpty()
+                  select new 
+                  {
+                      ProductName = p.Name,
+                      ProductColor = pf.Color, //Nullable hatası almamamızın nedeni ise string değerlerin referans tipli olmasından kaynaklanmasıdır.
+                      ProductHeight =(int?) pf.Height == null ? 0 : pf.Height //Eğer "(int?)" yazmazsak nullable hatası alırız.
+                  }
+                 ).ToList();
+
+
+    var rightJoinResuly = await (from pf in context.productFeatures
+                                 join p in context.Products on pf.Id equals p.Id into pList
+                                 from p in pList.DefaultIfEmpty()
+                                 select new
+                                 {
+                                     productName = p.Name,
+                                     productPrice = (decimal?)p.Price,
+                                     productColor = pf.Color,
+                                     productHeight =pf.Height
+                                 }).ToListAsync();
+
+    //LEFT - RIGHT JOIN END
 
     Console.WriteLine("İşlem Başarılı");
 
