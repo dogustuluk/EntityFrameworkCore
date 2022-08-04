35768630af476b4bb5f3aa55e34cbf8cbb4d0ac0
dogustuluk <dogus.tuluk@gmail.com>
2022-08-04 12:42:54 +0300

    transactions > SaveChanges

---
+++ EntityFrameworkCore.Transactions/Models/ProductFullForFunction2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.StoredProcedureAndFunction.Models
+{
+    public class ProductFullForFunction2
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string? Color { get; set; }
+        public int? Height { get; set; }
+        public int? Width { get; set; }
+    }
+}
---
+++ EntityFrameworkCore.Transactions/Program.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper.QueryableExtensions;
+using EntityFrameworkCore.CodeFirst;
+using EntityFrameworkCore.CodeFirst.DAL;
+using EntityFrameworkCore.Projections.DTOs;
+using EntityFrameworkCore.Projections.Mappers;
+using EntityFrameworkCore.Relationships.DAL;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+Initializer.Build();
+
+using (var context = new AppDbContext())
+{
+    //TRANSACTIONS START
+    //Kullanıldığı scope'a(alana) göre farklı anlamlara gelebilmektedir.
+    //Verinin bütünlüğünü sağlamaktadır. Ya hep ya hiç kuralına sahiptir. Eğer sıralı bir işlem yapılacak ise yani x kişisinin parası y kişisine transfer edilecek. X kişisinden paranın çekilmesinin sonucu olumlu ise ve y kişisine transfer işlemi başarılı olmadığında yapılmış olan tüm işlemler geri alınacaktır. Yani x kişisinden çekilen para da kişinin kendisine geri verilmiş olacaktır.
+    //SaveChanges start
+    var category = new Category() { Name = "Kılıflar" };
+    context.Categories.Add(category);
+
+    var product = context.Products.First();
+    product.Name = "Defter 005";
+    product.CategoryId = 45; //Bu noktada hata meydana geleceği için yeni bir kategori ekleme işlemi her ne kadar başarılı olarak gözükse de rollbag işlemi yapılır çünkü bu adımda bir hata meydana gelmektedir.
+    context.SaveChanges(); //EF Core bu iki işlemi tek bir çağrı ile yapıyor şuanda. Eğer iki işlemden herhangi birinde hata meydana gelirse otomatik olarak rollbag olayı meydana gelip yapılan işlemler geri alınıyor. 
+
+   //SaveChanges end
+   //TRANSACTIONS END
+    Console.WriteLine("İşlem Başarılı");
+
+}
+
+
+
+
