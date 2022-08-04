87e9f0bcad48f98789a964187da11007cc9637ae
dogustuluk <dogus.tuluk@gmail.com>
2022-08-04 14:24:23 +0300

    transactions > multiple DbContext instance

--- EntityFrameworkCore.Transactions/DAL/AppDbContext.cs
+++ EntityFrameworkCore.Transactions/DAL/AppDbContext.cs
@@ -4,22 +4,44 @@ using EntityFrameworkCore.Relationships.DAL;
 using EntityFrameworkCore.StoredProcedureAndFunction.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Data.Common;
 
 namespace EntityFrameworkCore.CodeFirst.DAL
 {
     public class AppDbContext:DbContext
     {
-       public DbSet<Product> Products { get; set; }
+        private DbConnection _connection;
+
+        public AppDbContext(DbConnection connection) //birden fazla instance var ise bu şekilde geçmemiz gerekiyor.
+        {
+            _connection = connection;
+        }
+        public AppDbContext()
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
        public DbSet<ProductFeature> productFeatures { get; set; }
        public DbSet<Category>  Categories { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
-            Initializer.Build();
-             optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information).UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
-          
+            if (_connection == default(DbConnection)) //tanımlanmamış ise
+            {
+                 Initializer.Build();
+                 optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information).UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
+
+            }
+            else
+            {
+                 optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information).UseSqlServer(_connection);
+
+            }
+            //Initializer.Build();
+            // optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information).UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
+
 
         }
         public override int SaveChanges()
--- EntityFrameworkCore.Transactions/Program.cs
+++ EntityFrameworkCore.Transactions/Program.cs
@@ -6,11 +6,18 @@ using EntityFrameworkCore.Projections.Mappers;
 using EntityFrameworkCore.Relationships.DAL;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+using System.Data.Common;
 using System.Linq;
 
 Initializer.Build();
 
-using (var context = new AppDbContext())
+var connection = new SqlConnection(Initializer.Configuration.GetConnectionString("SqlCon")); //AppDbContext'te "DbConnection" geçmemizin sebebi belki oracle connection geçebiliriz ya da başka connection'lar geçebiliriz diye. 
+
+IDbContextTransaction transaction = null; //EK
+
+using (var context = new AppDbContext(connection))
 {
     //TRANSACTIONS START
     //Kullanıldığı scope'a(alana) göre farklı anlamlara gelebilmektedir.
@@ -24,62 +31,81 @@ using (var context = new AppDbContext())
     //product.CategoryId = 45; //Bu noktada hata meydana geleceği için yeni bir kategori ekleme işlemi her ne kadar başarılı olarak gözükse de rollbag işlemi yapılır çünkü bu adımda bir hata meydana gelmektedir.
     //context.SaveChanges(); //EF Core bu iki işlemi tek bir çağrı ile yapıyor şuanda. Eğer iki işlemden herhangi birinde hata meydana gelirse otomatik olarak rollbag olayı meydana gelip yapılan işlemler geri alınıyor. 
 
+    //SaveChanges end
+
+
     //transaction 2 start
 
-    using (var transaction = context.Database.BeginTransaction())
+    using (/*var transaction*/ transaction = context.Database.BeginTransaction())
     {
+        //  var category = new Category() { Name = "Kılıflar" };
+        //  context.Categories.Add(category);
+        //  context.SaveChanges();
+        //  Product product = new()
+        //  {
+        //      Name = "Kılıf 1",
+        //      Price = 485,
+        //      Stock = 63,
+        //      Barcode = 47035,
+        //      DiscountPrice = 365,
+        //      CategoryId = category.Id //buradaki id alanı çalışmaz, ilk önce eklenmiş olması lazım ama şuan aynı anda ekleme işlemi yapılacak. Eğer bunu kullanmak istiyorsak kategori eklendikten sonra da SaveChanges dememiz lazım. Fakat ilk saveChanges sonrasında ikinci saveChanges ifadesinde bir hata olursa ilk veri ya da sonrasındaki veride hata oluşması durumunda veri tabanında bir tutarsızlık olacaktır. Bu durumda açık açık transaction belirtmemiz gerekecektir.
+        //                               //Önemli >>>>>>>>>>>>> Birden fazla saveChanges kullanırsak transaction yapısını, scope'unu kullanmamız gerekir.
+
+        //      //CategoryId = category.Id  şeklinde kullanmak doğru olmamaktadır. Bu yapıda transaction yapılmasına gerek yoktur. İlla id alınması gereken bir yer var ise yapılır. Ama bu yapıyı transaction kullanmadan da çözebiliriz. Not "#nonTransAction" isimli region'da belirtilmektedir.
+        //      //Transaction açıldığında her zaman trt-catch bloğu açılmamalı, ihtiyacımız varsa açılması. Eğer trt-catch açar isek catch bloğu içerisinde rollbag açık açık yazılmalıdır.
+
+        //  };
+
+        //  context.Products.Add(product);
+        //  context.SaveChanges();
+        ////  throw new Exception();
+        //  transaction.Commit(); //En sonda commit ettiğimiz için istediğimiz kadar saveChanges kullanabiliriz. Eğer 10 savechanges kullanıp sadece birinde hata çıksa dahi veri tabanında tutarsızlık olmayacak, transaction bunun yönetimini sağlayıp tüm işlemler geri alınacaktır.
+        //                          //Burada eğer bu transaction yapısını loglama yapmayacak isek try-catch bloğuna almamıza gerek yoktur.
+
+        //    #region nonTransAction
         var category = new Category() { Name = "Kılıflar" };
         context.Categories.Add(category);
         context.SaveChanges();
+
         Product product = new()
         {
-            Name = "Kılıf 1",
+            Name = "Kılıf 2",
             Price = 485,
             Stock = 63,
             Barcode = 47035,
             DiscountPrice = 365,
-            CategoryId = category.Id //buradaki id alanı çalışmaz, ilk önce eklenmiş olması lazım ama şuan aynı anda ekleme işlemi yapılacak. Eğer bunu kullanmak istiyorsak kategori eklendikten sonra da SaveChanges dememiz lazım. Fakat ilk saveChanges sonrasında ikinci saveChanges ifadesinde bir hata olursa ilk veri ya da sonrasındaki veride hata oluşması durumunda veri tabanında bir tutarsızlık olacaktır. Bu durumda açık açık transaction belirtmemiz gerekecektir.
-              //Önemli >>>>>>>>>>>>> Birden fazla saveChanges kullanırsak transaction yapısını, scope'unu kullanmamız gerekir.
-             
-            //CategoryId = category.Id  şeklinde kullanmak doğru olmamaktadır. Bu yapıda transaction yapılmasına gerek yoktur. İlla id alınması gereken bir yer var ise yapılır. Ama bu yapıyı transaction kullanmadan da çözebiliriz. Not "#nonTransAction" isimli region'da belirtilmektedir.
-            //Transaction açıldığında her zaman trt-catch bloğu açılmamalı, ihtiyacımız varsa açılması. Eğer trt-catch açar isek catch bloğu içerisinde rollbag açık açık yazılmalıdır.
-
+            URL = "adsadsad",
+            Category = category
         };
-
         context.Products.Add(product);
         context.SaveChanges();
-        throw new Exception();
-        transaction.Commit(); //En sonda commit ettiğimiz için istediğimiz kadar saveChanges kullanabiliriz. Eğer 10 savechanges kullanıp sadece birinde hata çıksa dahi veri tabanında tutarsızlık olmayacak, transaction bunun yönetimini sağlayıp tüm işlemler geri alınacaktır.
-                              //Burada eğer bu transaction yapısını loglama yapmayacak isek try-catch bloğuna almamıza gerek yoktur.
-
-        #region nonTransAction
-        //var category1 = new Category() { Name = "Kılıflar" };
-        //context.Categories.Add(category1);
-        //Product product1 = new()
-        //{
-        //    Name = "Kılıf 1",
-        //    Price = 485,
-        //    Stock = 63,
-        //    Barcode = 47035,
-        //    DiscountPrice = 365,
-        //    Category = category1
-        //};
-        //context.Products.Add(product1);
-        //context.SaveChanges();
-
-        #endregion
-
-
+        // transaction.Commit();
+        //    #endregion
 
 
         //transaction 2 end
 
-        //SaveChanges end
-        //TRANSACTIONS END
-        Console.WriteLine("İşlem Başarılı");
+        //Multiple DbContext Instance Start
+        //Önce DbConnection'ı almamız gerekmektedir AppDbContext içerisinde.
+        using (var dbContext2 = new AppDbContext(connection)) //Bu yapıyı scope'un dışında da kullanabiliriz. Bu noktada ek kodlar da yazmamız gerekmektedir. ek kod >> #EK olarak adlandırılıyor.
+        {
+            dbContext2.Database.UseTransaction(transaction.GetDbTransaction()); //her iki context'i de kullanmaya başlıyoruz bu kod ile.
+
+            var productMultiple = dbContext2.Products.First();
+            productMultiple.Stock = 1000;
+            dbContext2.SaveChanges();
+
+        }
+
+
+        transaction.Commit();
+        //Multiple DbContext Instance End
+
+    }
+
+
+
+    //TRANSACTIONS END
+    Console.WriteLine("İşlem Başarılı");
 
 }
-
-
-
-
