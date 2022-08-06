e46c254d881e7ef4722ddccff7380ecc48e0f189
dogustuluk <dogus.tuluk@gmail.com>
2022-08-06 15:04:31 +0300

    UserController created

--- Concurrency.Web/Controllers/UserController.cs
+++ Concurrency.Web/Controllers/UserController.cs
@@ -17,5 +17,48 @@ namespace Concurrency.Web.Controllers
         {
             return View(await _context.Users.ToListAsync());
         }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(User user)
+        {
+            try
+            {
+                _context.Users.Update(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(List));
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                var exceptionEntry = exception.Entries.First();
+
+                var currentUser = exceptionEntry.Entity as User;
+
+                var databaseValue = exceptionEntry.GetDatabaseValues();
+
+                var clientValues = exceptionEntry.CurrentValues;
+
+                if (databaseValue == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Bu kullanıcı başka bir admin tarafından silindi");
+                }
+                else
+                {
+                    var databaseUser = databaseValue.ToObject() as User;
+                    ModelState.AddModelError(string.Empty, "Bu kullanıcı admin tarafından güncellenmiştir");
+                    ModelState.AddModelError(string.Empty, $"Güncellenen Değer: Username:{databaseUser.Username}, Phone:{databaseUser.Phone}, Birthday:{databaseUser.Birthday}");
+                }
+
+                return View(user);
+            }
+
+            
+        }
     }
 }
