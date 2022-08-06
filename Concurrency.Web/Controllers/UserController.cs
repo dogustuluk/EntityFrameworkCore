bb78eab150e9f94be32d19996154a36067f68041
dogustuluk <dogus.tuluk@gmail.com>
2022-08-06 14:54:23 +0300

    userController > list method

---
+++ Concurrency.Web/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Concurrency.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Concurrency.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+    }
+}
