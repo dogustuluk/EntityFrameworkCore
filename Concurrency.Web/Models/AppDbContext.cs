e01ae0afe7a856373da1c5c00464c6899920322f
dogustuluk <dogus.tuluk@gmail.com>
2022-08-06 14:50:21 +0300

    User created

--- Concurrency.Web/Models/AppDbContext.cs
+++ Concurrency.Web/Models/AppDbContext.cs
@@ -10,12 +10,14 @@ namespace Concurrency.Web.Models
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Fluent API
             modelBuilder.Entity<Product>().Property(x => x.RowVersion).IsRowVersion(); //2.yol, best practise.
             modelBuilder.Entity<Product>().Property(x => x.Price).HasPrecision(18, 2);
 
+            modelBuilder.Entity<User>().Property(x => x.RowVersion).IsRowVersion();
 
             base.OnModelCreating(modelBuilder);
         }
---
+++ Concurrency.Web/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Concurrency.Web.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string? Phone { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public byte[] RowVersion { get; set; }
+    }
+}
