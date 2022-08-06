91fbc61301a1c830ba0b6928bf970c2895bd5073
dogustuluk <dogus.tuluk@gmail.com>
2022-08-06 14:46:35 +0300

    DbUpdateConcurrencyException

--- Concurrency.Web/Controllers/ProductsController.cs
+++ Concurrency.Web/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Concurrency.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace Concurrency.Web.Controllers
 {
@@ -31,10 +32,43 @@ namespace Concurrency.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Update(Product product)
         {
-            _context.Products.Update(product);
-            await _context.SaveChangesAsync();
-            
-            return RedirectToAction(nameof(List));
+            try
+            {
+                _context.Products.Update(product);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(List));
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                var exceptionEntry = exception.Entries.First(); //İlgili satırı alıyoruz
+
+                var currentProduct = exceptionEntry.Entity as Product;   //kullanıcının girmiş olduğu değerleri alıyoruz //güncel değerdir.
+
+                var databaseValues = exceptionEntry.GetDatabaseValues(); //veri tabanındaki verileri alıyoruz
+
+              
+
+                var clientValues = exceptionEntry.CurrentValues;
+
+
+                if (databaseValues == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından silindi");
+                }
+                else
+                {
+                    var databaseProduct = databaseValues.ToObject() as Product; //veri tabanındaki değerleri bir objeye çevirir ve bu objeler bir Product objesi olacak
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından güncellendi");
+                    ModelState.AddModelError(string.Empty, $"Güncellenen Değer: Name:{databaseProduct.Name}, Price:{databaseProduct.Price}, Stock:{databaseProduct.Stock}");
+                }
+
+                return View(product);
+            }
+
+
+
+         
         }
     }
 }
